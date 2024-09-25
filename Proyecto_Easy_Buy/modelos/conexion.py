021763f2fc25c22ffd83bab7849cb096d17026f1
Aldo Ray Vasquez Lopez <avasquezl@ulasalle.edu.pe>
2024-09-24 20:48:16 -0500

    Subiendo conexion a la base de datos

---
+++ Proyecto_Easy_Buy/modelos/conexion.py
@@ -0,0 +1,22 @@
+import pyodbc
+
+
+def aspectos_bd():
+    with open("ambiente/configuracion.txt", "r") as aspectos:
+        return [line.strip() for line in aspectos.readlines() if line.strip()]
+
+
+def conexion():
+    lista_conexion = aspectos_bd()
+    try:
+        con = pyodbc.connect(
+            f'Driver={lista_conexion[0]};'
+            f'SERVER={lista_conexion[1]};'
+            f'DATABASE={lista_conexion[2]};'
+            f'Trusted_Connection={lista_conexion[3]}'
+        )
+        return con
+
+    except Exception as e:
+        print(f'Error en la conexión: {str(e)}')
+        return None
